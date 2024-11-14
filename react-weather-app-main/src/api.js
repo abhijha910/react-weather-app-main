db4e98a19674eef5e7bd185e6cca0632c29f47c0
Abhyanand Jha <abhyanandlsc@gmail.com>
2024-11-14 11:04:03 +0530

    Initial commit

---
+++ react-weather-app-main/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5824646f50msh23c948150c14b83p10114ejsn5f71f521b02d",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ab672d5049a426b7c0da994ae1d6e834"; // enter your key from openweather API
